5b14831958ac0a2d9821e00475dddbbc73ee5877
Ruben Schmidmeister <ruben.schmidmeister@messerli.ch>
2020-09-30 11:18:59 +0200

    Add method for adding all converters

--- Funcky.NewtonsoftJson/JsonSerializerSettingsExtensions.cs
+++ Funcky.NewtonsoftJson/JsonSerializerSettingsExtensions.cs
@@ -4,6 +4,8 @@ namespace Funcky.NewtonsoftJson
 {
     public static class JsonSerializerSettingsExtensions
     {
+        public static JsonSerializerSettings AddFunckyConverters(this JsonSerializerSettings settings) => settings.AddOptionConverter();
+
         public static JsonSerializerSettings AddOptionConverter(this JsonSerializerSettings settings)
         {
             settings.Converters.Add(new JsonOptionConverter());
